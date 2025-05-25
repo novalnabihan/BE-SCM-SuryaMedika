2117822a5792758adf1b87bf8880fa7438fadb32
noval <novalnabihan@gmail.com>
2025-05-26 03:41:43 +0800

    warehouse addition - latest version using prisma orm

--- src/db.js
+++ src/db.js
@@ -5,14 +5,14 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-console.log("PG Config", {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+// console.log("PG Config", {
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
+// console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
 module.exports = pool;
