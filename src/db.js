4864d968c65c13b8e7d07e312b45f97a24ca5373
noval <novalnabihan@gmail.com>
2025-05-26 02:18:22 +0800

    feat: employee addition - latest version using Prisma ORM

--- src/db.js
+++ src/db.js
@@ -1,7 +1,11 @@
+require('dotenv').config(); // HARUS ada sebelum pakai process.env
 const { Pool } = require('pg');
 
-
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+console.log("PG Config", {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
@@ -9,5 +13,6 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
 module.exports = pool;
